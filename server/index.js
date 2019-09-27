d4e335680085c7a49e70d6aca36f461723a41113
Lyndsey Reese <lmariereese@gmail.com>
2019-09-27 15:39:30 -0400

    Debug: api routes with heroku

--- server/index.js
+++ server/index.js
@@ -65,23 +65,12 @@ const createApp = () => {
   app.use(passport.initialize())
   app.use(passport.session())
 
-  // auth and api routes
-  app.use('/auth', require('./auth'))
-  app.use('/api', require('./api'))
-
   // static file-serving middleware
   app.use(express.static(path.join(__dirname, '..', 'public')))
 
-  // any remaining requests with an extension (.js, .css, etc.) send 404
-  app.use((req, res, next) => {
-    if (path.extname(req.path).length) {
-      const err = new Error('Not found')
-      err.status = 404
-      next(err)
-    } else {
-      next()
-    }
-  })
+  // auth and api routes
+  app.use('/auth', require('./auth'))
+  app.use('/api', require('./api'))
 
   // sends index.html
   app.use('*', (req, res) => {
@@ -113,6 +102,17 @@ const createApp = () => {
     }
     await getData()()
     console.log('Finished cron job')
+  })
+
+  // any remaining requests with an extension (.js, .css, etc.) send 404
+  app.use((req, res, next) => {
+    if (path.extname(req.path).length) {
+      const err = new Error('Not found')
+      err.status = 404
+      next(err)
+    } else {
+      next()
+    }
   })
 
   // error handling endware
