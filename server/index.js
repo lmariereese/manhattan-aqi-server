9e0c5bd4bbf770efe746211b89a1827599ec9277
Lyndsey Reese <lmariereese@gmail.com>
2019-09-27 15:50:30 -0400

    Troubleshooting

--- server/index.js
+++ server/index.js
@@ -65,12 +65,23 @@ const createApp = () => {
   app.use(passport.initialize())
   app.use(passport.session())
 
-  // static file-serving middleware
-  app.use(express.static(path.join(__dirname, '..', 'public')))
-
   // auth and api routes
   app.use('/auth', require('./auth'))
   app.use('/api', require('./api'))
+
+  // static file-serving middleware
+  app.use(express.static(path.join(__dirname, '..', 'public')))
+
+  // any remaining requests with an extension (.js, .css, etc.) send 404
+  app.use((req, res, next) => {
+    if (path.extname(req.path).length) {
+      const err = new Error('Not found')
+      err.status = 404
+      next(err)
+    } else {
+      next()
+    }
+  })
 
   // sends index.html
   app.use('*', (req, res) => {
@@ -102,17 +113,6 @@ const createApp = () => {
     }
     await getData()()
     console.log('Finished cron job')
-  })
-
-  // any remaining requests with an extension (.js, .css, etc.) send 404
-  app.use((req, res, next) => {
-    if (path.extname(req.path).length) {
-      const err = new Error('Not found')
-      err.status = 404
-      next(err)
-    } else {
-      next()
-    }
   })
 
   // error handling endware
