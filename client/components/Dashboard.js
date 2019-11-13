65032b43244c10ad80841cffc111542c46677a63
Lyndsey Reese <lmariereese@gmail.com>
2019-11-13 14:16:50 -0500

    Dashboard component: connected component to redux store

--- client/components/Dashboard.js
+++ client/components/Dashboard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {getCurrentWeather} from '../store/weather';
+import {getCurrentPollution} from '../store/pollution';
 
 class Dashboard extends React.Component {
   // constructor(props) {
@@ -8,24 +9,27 @@ class Dashboard extends React.Component {
   // }
   componentDidMount() {
     this.props.getCurrentWeather();
+    this.props.getCurrentPollution();
   }
 
   render() {
     return (
       <div className="wrapper">
         <h2>Dashboard</h2>
-        {this.props.currentWeather ? <p>{this.props.currentWeather}</p> : ''}
+        {/* {this.props.currentWeather ? <p>{this.props.currentWeather}</p> : ''} */}
       </div>
     );
   }
 }
 
 const mapStateToProps = state => ({
-  currentWeather: state.weather.currentWeather
+  currentWeather: state.weather.current,
+  currentPollution: state.pollution.current
 });
 
 const mapDispatchToProps = dispatch => ({
-  getCurrentWeather: () => dispatch(getCurrentWeather())
+  getCurrentWeather: () => dispatch(getCurrentWeather()),
+  getCurrentPollution: () => dispatch(getCurrentPollution())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
